69f37a13a1e0657b983bbd8a355ef7c661ab616b
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-03-30 01:07:10 +0200

    .

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        // User::create([
+        //     'name' => 'John Doe',
+        //     'email' => 'johndoe@example.com',
+        //     'password' => Hash::make('mypassword'), // Replace 'mypassword' with your desired password
+        //     'avatar_image'=>'abc',
+        //     'national_id'=>'1234',
+        //     "date_of_birth"=> now(),
+        //     'gender'=>1,
+        //     'phone'=>123,
+        // ]);
 
     }
 }
