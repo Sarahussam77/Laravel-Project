cf5fd119a306ca01ac6a28f3ba904c41beb46d03
sara hussam <sarahussam203@gmail.com>
2023-03-30 20:20:08 +0200

    defineroles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
@@ -13,8 +14,21 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        User::factory(1)->create();
+    { 
+        $admin= User::create([
+            'name' => 'admin',
+            'password' =>Hash::make(123456) ,
+            'avatar_image'=>'admin.jpg',
+            'national_id'=>'5394267842450618',
+            'email' => 'admin@admin.com',
+            'date_of_birth'=>'1998-12-18',
+            'gender'=>1,
+            'phone'=>'01273296544',
+            'email_verified_at' => now(),
+           
+            
+        ]);
+        $admin->assignRole('admin');
 
     }
 }
