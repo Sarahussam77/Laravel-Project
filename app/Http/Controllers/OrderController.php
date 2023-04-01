d711d6b7e6d7d6e5f4e6f278cc22e69e5513271b
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-02 00:40:38 +0200

    edit order curd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,6 +10,7 @@ use App\Models\Doctor;
 use App\Models\Medicine;
 use App\Models\Pharmacy;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
 
 use Phar;
@@ -23,7 +24,7 @@ class OrderController extends Controller
    public function index(Request $request)
    {  
      if ($request->ajax()) {
-       $data = Order::select('*')->get();
+       $data = Order::select('id','user_id','doctor_id','pharmacy_id','status' , 'is_insured','creator_type', 'price')->get();
        return Datatables::of($data)->addIndexColumn()
            ->addColumn('action', function ($row) {
             $button = '<a name="show" id="'.$row->id.'" class="show btn btn-success btn-sm p-0" href="'.route('orders.show', $row->id).'" style="border-radius: 20px;"><i class="fas fa-eye m-2"></i></a>';
@@ -35,7 +36,18 @@ class OrderController extends Controller
         </button>
         </form>';
             return $button;
-            ;
+        })
+        ->addColumn('Pharmacy', function($row){
+            $Pharmacyname = Pharmacy::all()->where('id' , $row['pharmacy_id'] )->first()->name;
+            return $Pharmacyname;
+        })
+        ->addColumn('doctor', function($row){
+            $doctorname = Doctor::all()->where('id' , $row['doctor_id'] )->first()->name;
+            return $doctorname;
+        })
+        ->addColumn('user', function($row){
+            $username = User::all()->where('id' , $row['user_id'] )->first()->name;
+            return $username;
         })
            ->rawColumns(['action'])
            ->make(true);
