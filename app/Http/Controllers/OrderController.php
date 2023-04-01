dcf0cf2b84854396699a2c760085353658f74912
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-01 20:38:50 +0200

    order curd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\DataTables\OrdersDataTable;
+use App\Models\Address;
+use App\Models\Doctor;
+use App\Models\Medicine;
+use App\Models\Pharmacy;
 use App\Models\User;
 use DataTables;
+use Phar;
+
 class OrderController extends Controller
 {
     /**
@@ -17,12 +23,18 @@ class OrderController extends Controller
    {   if ($request->ajax()) {
        $data = Order::select('*')->get();
        return Datatables::of($data)->addIndexColumn()
-           ->addColumn('action', function($row){
-               $btn = '<a href="/orders/'.$row->id.'" class="btn btn-primary btn-sm">View</a>'." ".
-               '<a href="//'.$row->id.'" class="btn btn-primary btn-sm">Edit</a>'.'<br>'.
-               '<a href="//'.$row->id.'" class="btn btn-primary btn-sm">Delete</a>';
-               return $btn;
-           })
+           ->addColumn('action', function ($row) {
+            $button = '<a name="show" id="'.$row->id.'" class="show btn btn-success btn-sm p-0" href="'.route('orders.show', $row->id).'" style="border-radius: 20px;"><i class="fas fa-eye m-2"></i></a>';
+            $button .= '<a name="edit" id="'.$row->id.'" class="edit btn btn-primary btn-sm p-0" href="'.route('orders.edit', $row->id).'" style="border-radius: 20px;"><i class="fas fa-edit m-2"></i></a>';
+            $button .= '<form method="post" action= "'.route('orders.destroy', $row->id).'">
+        <input type="hidden" name="_token" value="'. csrf_token().' ">
+        <input type="hidden" name="_method" value="delete">
+        <button type="submit" class="btn btn-danger btn-sm  p-0 ml-3" style="border-radius: 20px;"><i class="fas fa-trash m-2"></i>
+        </button>
+        </form>';
+            return $button;
+            ;
+        })
            ->rawColumns(['action'])
            ->make(true);
    }
@@ -34,7 +46,17 @@ class OrderController extends Controller
      */
     public function create()
     {
-        return view("Orders.create");
+        $users = User::all();
+        $doctors = Doctor::all();
+        $medicine = Medicine::all();
+        $pharmacy = Pharmacy::all();
+        $address = Address::all();
+        return view("Orders.create",[
+        'medicine'=>$medicine ,
+        'pharmacy'=>$pharmacy ,
+        'doctors'=>$doctors,
+        'address'=>$address,
+        'users' => $users]);
     }
 
     /**
@@ -42,7 +64,29 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        $UserId = User::all()->where('name' , $data['name_of_user'] )->first()->id;
+        $DocId = Doctor::all()->where('name' , $data['DocName'] )->first()->id;
+        $PharmacyId = Pharmacy::all()->where('name' , $data['PharmacyName'] )->first()->id;
+        $useradd = Address::all()->where('street_name' , $data['address'] )->first()->id;
+        // dd($data);
+        $med = $data['med'];
+        $qty = $data['qty'];
+        
+        $order = Order::Create([
+            'status'=> $data['status'],
+            'pharmacy_id'=> $PharmacyId,
+            'user_id'=> $UserId,
+            'doctor_id'=> $DocId,
+            'is_insured'=> $data['insured'],
+            'creator_type'=>$data['creator_type'],
+            'user_address_id'=>$useradd,
+            'actions' => '--'
+        ]);
+
+
+
+        return to_route('orders.index');
     }
 
     /**
@@ -50,11 +94,17 @@ class OrderController extends Controller
      */
     public function show(string $id)
     {
-        $orders= Order::find($id);
-        $users = User::all();
+        $order= Order::find($id);
+        $user = User::all();
+        $medicine = Medicine::all();
+        $pharmacy = Pharmacy::all();
+        $doctor = Doctor::all();
         return view('orders.show', [
-            'orders' => $orders,
-            'users' => $users,
+            'order' => $order,
+            'user' => $user,
+            'medicine' => $medicine,
+            'pharmacy' => $pharmacy,
+            'doctor' => $doctor,
     
         ]);
     }
@@ -64,7 +114,22 @@ class OrderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $order= Order::find($id);
+        $users = User::all();
+        $doctors = Doctor::all();
+        $medicine = Medicine::all();
+        $pharmacy = Pharmacy::all();
+        $address = Address::all();
+
+        return view("Orders.edit",[ 
+            'order' => $order,
+            'medicine'=>$medicine ,
+            'users' => $users,
+            'pharmacy'=>$pharmacy ,
+            'doctors'=>$doctors,
+            'address'=>$address,
+    
+    ]);
     }
 
     /**
@@ -72,14 +137,33 @@ class OrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->all();
+        $UserId = User::all()->where('name' , $data['name_of_user'] )->first()->id;
+        $DocId = Doctor::all()->where('name' , $data['DocName'] )->first()->id;
+        $PharmacyId = Pharmacy::all()->where('name' , $data['PharmacyName'] )->first()->id;
+        $useradd = Address::all()->where('street_name' , $data['address'] )->first()->id;
+        $order = Order::findOrFail($id);
+        
+        $order->status =$data['status'];
+        $order->pharmacy_id = $PharmacyId;
+        $order->user_id = $UserId;
+        $order->doctor_id = $DocId;
+        $order->is_insured = $request->input('insured');
+        $order->creator_type = $request->input('creator_type');
+        $order->user_address_id = $useradd;
+        $order->actions = "--";
+
+        $order->save();
+        return to_route('orders.index');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy( String $id)
     {
-        //
+        Order::findOrFail($id)->delete();
+        return redirect()->route('orders.index')->with('success','Record deleted successfully');
     }
 }
--- resources/views/Orders/edit.blade.php
+++ resources/views/Orders/edit.blade.php
@@ -1,40 +1,103 @@
 @extends('layouts.app')
 
 @section('title')
-    edit Order
+    Create Order
 @endsection
 
 @section('content')
 
 
-    <form action="#" enctype="multipart/form-data">
+    <form action="{{route('orders.update',$order['id'])}}" method="POST">
+    @method('PUT')
         @csrf
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">User</label>
+            <label for="name_of_user1" class="form-label">User</label>
             <select name="name_of_user" class="form-control">
-                    <option >User One</option>
-                    <option >User Two</option>
+                    @foreach($users as $user)
+                    <option>{{$user->name}}</option>
+                    @endforeach
+                  </select>
             </select>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Medicine Name</label>
-            <input name="medicine_name" type="text" class="form-control" id="exampleFormControlInput1">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Quantity</label>
-            <textarea name="quantity" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Type</label>
-            <textarea name="type" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Price</label>
-            <textarea name="price" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="address" class="form-label">Address :street name</label>
+            <select name="address" class="form-control">
+                    @foreach($address as $add)
+                    <option>{{$add->street_name}}</option>
+                    @endforeach
+                  </select>
+            </select>
         </div>
         
+        <div class="mb-3" >
+                  <label for="insured">Is insured ?</label>
+                  <select name="insured" class="form-control">
+                    
+                    <option>Yes</option>
+                    <option>No</option>
+                   
+                  </select>
+        </div>
+        <div class="mb-3">
+            <label for="med" class="form-label">Medicine Name</label>
+            <select class="form-control" name="med" >   
+                @foreach($medicine as $med)
+                    <option value="{{$med->id}}">{{$med->name}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="qty" class="form-label">Quantity</label>
+            <select class="form-control" name="qty" >
+                        @for($x=1;$x<=10;$x++)
+                            <option value="{{$x}}">{{$x}}</option>
+                        @endfor
+                        </select>
+        </div>
+        <div class="mb-3">
+        <label for="DocName">Doctor Name</label>
+            <select name="DocName" class="form-control" >
+                @foreach($doctors as $doctor)
+                    <option>{{$doctor->name}}</option>
+                 @endforeach
+            </select>
+        </div>
+        <!-- <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Price</label>
+            <input name="price" class="form-control" id="exampleFormControlTextarea1" rows="3"></input>
+        </div> -->
+        <div class="mb-3">
+            <label for="status" class="form-label">Status</label>
+            <select name="status" class="form-control" >
+                <option>NEW</option>
+                <option>Processing</option>
+                <option>WaitingForUserConfirmation</option>
+                <option>Canceled</option>
+                <option>Confirmed</option>
+                <option>Delivered</option>
+
+                
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="creator_type" class="form-label">Creator Type</label>
+            <select name="creator_type" class="form-control" >
+                <option>Doctor</option>
+                <option>Pharmacy</option>
+                <option>User</option>
+            </select>
+        </div>
+        <div class="form-group">
+                        <label for="PharmacyName">Pharmacy Name</label>
+                        <select name="PharmacyName" class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">
+                          @foreach($pharmacy as $phar)
+                          <option>{{$phar->name}}</option>
+                          @endforeach
+                        </select>
+        </div>
+            
         
 
-        <button class="btn btn-success" style="background-color:#6D9886 ; color:white">Submit</button>
+        <button class="btn btn-success" style="background-color:#6D9886 ; color:white">Update</button>
     </form>
 @endsection
