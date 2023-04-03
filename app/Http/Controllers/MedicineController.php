afb3540c4a677c62baadb7c8aafcb9e71c61fdf4
sara hussam <sarahussam203@gmail.com>
2023-04-03 21:44:24 +0200

    medicinecruds

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -15,12 +15,20 @@ class MedicineController extends Controller
     public function index(Request $request)
     {    
          if ($request->ajax()) {
-        $data = Medicine::select('id','name','quantity','type','price','pharmacy_id','doctor_id')->get();
+        $data = Medicine::select('id','name','type','price')->get();
         return DataTables::of($data)->addIndexColumn()
-            ->addColumn('action', function($row){
-                $btn = '<a href="javascript:void(0)" class="btn btn-primary btn-sm">View</a>';
-                return $btn;
-            })
+        ->addColumn('action', function ($row) {
+            $button = '<a name="show" id="'.$row->id.'" class="show btn btn-success btn-sm p-0 mr-2" href="'.route('medicines.show', $row->id).'" style="border-radius: 20px;"><i class="fas fa-eye m-2"></i></a>';
+            $button .= '<a name="edit" id="'.$row->id.'" class="edit btn btn-primary btn-sm p-0 mr-2" href="'.route('medicines.edit', $row->id).'" style="border-radius: 20px;"><i class="fas fa-edit m-2"></i></a>';
+            $button .= '<form method="post" action= "'.route('medicines.destroy', $row->id).'">
+        <input type="hidden" name="_token" value="'. csrf_token().' ">
+        <input type="hidden" name="_method" value="delete">
+        <button type="submit" class="btn btn-danger btn-sm  p-0 ml-4" style="border-radius: 20px;"><i class="fas fa-trash m-2"></i>
+        </button>
+        </form>';
+            return $button;
+            ;
+        })
             ->rawColumns(['action'])
             ->make(true);
     }
@@ -33,15 +41,22 @@ class MedicineController extends Controller
      */
     public function create()
     {
-        //
+        return view("Medicines.create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+    {   $data = $request->all();
+        Medicine::create([
+            'name'=>$data['name'],
+            'price'=>$data['price'],
+            'type'=>$data['type'],
+            
+
+        ]);
+        return to_route('medicines.index');
     }
 
     /**
@@ -49,7 +64,8 @@ class MedicineController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $medicine= Medicine::find($id);
+        return view('medicines.show', ['medicine' => $medicine]);
     }
 
     /**
@@ -57,7 +73,9 @@ class MedicineController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $medicine =Medicine::find($id);
+        
+        return view('medicines.edit', ['medicine' => $medicine]);
     }
 
     /**
@@ -65,7 +83,14 @@ class MedicineController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $medicine = Medicine::findOrFail($id);
+        $medicine->name = $request->input('name');
+        $medicine->type = $request->input('type');
+        $medicine->price= $request->input('price');
+        
+        $medicine->save();
+     
+        return redirect()->route('medicines.index');
     }
 
     /**
@@ -73,6 +98,15 @@ class MedicineController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $medicine=Medicine::find($id);
+        if(count($medicine->orders))
+        {
+           $alert=[];
+           $alert['type']='danger';
+           $alert['message']='medicine is in orders Can not be deleted';
+           return view('medicines.index',['alert'=>$alert]);
+        }
+        $medicine->delete();
+        return redirect()->route('medicines.index');
     }
 }
--- resources/views/Medicines/create.blade.php
+++ resources/views/Medicines/create.blade.php
@@ -7,19 +7,13 @@
 @section('content')
 
 
-    <form action="#" enctype="multipart/form-data">
+    <form method="POST" action= "{{route('medicines.store')}}"  enctype="multipart/form-data">
         @csrf
        
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Name</label>
             <input name="name" type="text" class="form-control" id="exampleFormControlInput2">
         </div>
-
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Quantity</label>
-            <input name="quantity" type="text" class="form-control" id="exampleFormControlInput1">
-        </div>
-
         <div class="mb-3">
             <label for="exampleFormControlTextarea1" class="form-label">Type</label>
             <input name="type" type="text" class="form-control" id="exampleFormControlInput1">
