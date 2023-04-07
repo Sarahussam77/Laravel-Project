4bb6990597b4bcb14a6bb8ad4e1fd6fc4c989856
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-07 13:57:38 +0200

    address conroller api edit

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Auth;
 class AddressController extends Controller
 {
     public function index () {
-
-        // $userId = Auth::user()->id; replace wtih one
-        $userAddresses = Address::where('user_id','1')->first();
+        $userId = Auth::user()->typeable_id;
+        $userAddresses = Address::where('user_id',$userId)->first();
         return $userAddresses;
 
     }
@@ -43,7 +42,8 @@ class AddressController extends Controller
         return new AddressResource ($address);
     }
     public function show (Address $address) {
-        if ( $address->user->id != '2') {
+        $userId = Auth::user()->typeable_id;
+        if ( $address->user->typeable_id != $userId ) {
             return response()->json(["message" => "This user does not have this address please provide a valid address id for this user"],404);
         }
         else {
@@ -52,12 +52,12 @@ class AddressController extends Controller
     }
 
     public function update (Address $address , UpdateAddressRequest $request){
-        // $userId = Auth::user()->id;
-        if ( $address->user->id != '1' ) {
+        $userId = Auth::user()->typeable_id;
+        if ( $address->user->typeable_id != $userId ) {
             return response()->json(["message" => "This user does not have this address please provide a valid address id for this user"],404);
         }
         else {
-            $previousAddressIsMain = Address::where('user_id','1')->where('is_main', 1)->first();
+            $previousAddressIsMain = Address::where('user_id',$userId)->where('is_main', 1)->first();
             if ( $request->input('is_main') == 1 && !empty($previousAddressIsMain) ) {
                 $previousAddressIsMain->is_main = 0;
                 $previousAddressIsMain->save();
@@ -69,7 +69,7 @@ class AddressController extends Controller
                 'flat_number' => $request->flat_number,
                 'is_main' => $request->is_main,
                 'area_id' => $request->area,
-                'user_id' =>'1'
+                'user_id' =>$userId
             ]);
 
         }
@@ -77,7 +77,8 @@ class AddressController extends Controller
 
 
     public function destroy (Address $address) {
-        if ( $address->user->id != '1') {
+        $userId = Auth::user()->typeable_id;
+        if ( $address->user->typeable_id != $userId) {
             return response()->json(["message" => "This user does not have this address please provide a valid address id for this user"],404);
         }
         else{
